33e87727c5455a5145f06b7056e10ce1595de14e
ashiqee <ashiq.buet73@gmail.com>
2024-01-22 00:20:17 +0600

    color and update

--- src/components/shared/Rightbar/Rightbar.jsx
+++ src/components/shared/Rightbar/Rightbar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Rightbar = () => {
     return (
-        <div className='w-32 border min-h-screen'>
+        <div className='w-56 bg-pink-50 shadow-2xl min-h-[100vh] flex-end '>
             Right Bar
         </div>
     );
