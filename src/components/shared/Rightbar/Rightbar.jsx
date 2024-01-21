17b8507848af9a5fc7786b507e0f70a6a077974a
ashiqee <ashiq.buet73@gmail.com>
2024-01-22 01:05:49 +0600

    make responsive

--- src/components/shared/Rightbar/Rightbar.jsx
+++ src/components/shared/Rightbar/Rightbar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Rightbar = () => {
     return (
-        <div className='w-56 bg-pink-50 shadow-2xl min-h-[100vh] flex-end '>
+        <div className='w-56 hidden lg:block fixed -bottom-28 end-20 bg-pink-50 shadow-2xl min-h-[100vh] flex-end '>
             Right Bar
         </div>
     );
--- src/components/shared/Sidebar.jsx
+++ src/components/shared/Sidebar.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const Sidebar = () => {
     return (
-        <div className="w-56 bg-[#FFFFFF] shadow-2xl  rounded-tl-2xl min-h-screen">
+        <div className="w-56 hidden md:block fixed start-20 bg-[#FFFFFF] shadow-2xl  rounded-tl-2xl min-h-screen">
           
 
         <div className="p-4">
