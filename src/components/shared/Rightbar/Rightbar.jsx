9033a6cd3bd5c425ad3556d5ceb4139dbc30beb2
sabrinara <saashid18@gmail.com>
2024-01-22 18:15:42 +0600

    location component added

--- src/components/shared/Rightbar/Rightbar.jsx
+++ src/components/shared/Rightbar/Rightbar.jsx
@@ -1,11 +1,11 @@
+import Location from "@/components/Location/Location";
 import React from "react";
 
 const Rightbar = () => {
   return (
-    <div className="w-60 hidden lg:block fixed -bottom-16 end-0 bg-pink-50 shadow-2xl min-h-[100vh] flex-end ">
-      Right Bar
+    <div className="w-64 hidden lg:block fixed -bottom-16 end-0 bg-pink-50 shadow-2xl min-h-[100vh] flex-end ">
+      <Location />
     </div>
   );
 };
-
 export default Rightbar;
