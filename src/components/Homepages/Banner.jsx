016cd0d5d74d0412b45b5e927ecb919c59e57f16
husneara <husnearaa99@gmail.com>
2024-01-22 21:16:49 +0600

    has completed banner and FAQ section

--- src/components/Homepages/Banner.jsx
+++ src/components/Homepages/Banner.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+
+import { MdArrowOutward } from "react-icons/md";
+import Image from 'next/image';
+
 
 const Banner = () => {
-  return <div>Banner</div>;
+
+  return (
+    <div>
+
+      <div className='w-full h-[50vh] my-10 rounded-lg bg-gradient-to-r from-pink-200 to-pink-100  mx-auto'>
+        <div className='flex items-center justify-between'>
+          <div className='pl-10 '>
+            <h2 className='font-semibold text-3xl'>10% Discount</h2>
+            <p>Get a discount on certain days and don't miss it</p>
+            <button className='rounded-full bg-[#FF385D] text-white text-3xl'><MdArrowOutward /></button>
+          </div>
+          <div>
+            <div className='flex items-center justify-center' >
+              <Image width={400} height={100} className='w-full h-full' src={"https://i.postimg.cc/dQTr9xpk/banner2-removebg-preview.png"} alt='picture' />
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+    </div>
+  );
 };
 
 export default Banner;
+
