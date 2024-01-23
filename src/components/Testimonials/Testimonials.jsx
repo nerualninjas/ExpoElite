049bd2bd4073e6015822d2ccad65a16dd66c0b61
sabrinara <saashid18@gmail.com>
2024-01-23 12:28:47 +0600

    Aos and some changes in testimonial section

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -2,37 +2,41 @@
 
 import Image from 'next/image';
 import React, { useEffect, useState } from 'react';
-
+import AOS from 'aos'; 
+import 'aos/dist/aos.css';
 const Testimonials = () => {
   const [persons, setPersons] = useState([]);
 
   useEffect(() => {
-    // Fetch data directly from the JSON file
     fetch('/testimonial.json')
       .then(res => res.json())
       .then(data => setPersons(data))
+
+      AOS.init();
   }, []);
 
   return (
-    <div className='text-center bg-sky-100  my-10 py-10 '>
+    <div className='text-center   my-10 py-10 '>
       <h1 className='text-4xl mt-10 font-bold text-sky-950'>Our Testimonials</h1>
       <p className='mt-6'>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
 
-     <div className='grid grid-cols-1 md:grid-cols-3 pt-10  gap-4 place-items-center'>
+     <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 pt-10 gap-4 place-items-center ' >
      {persons.map((person, index) => (
-        <div key={index} className='flex flex-col justify-center items-center w-60 h-72 rounded border-2 shadow-2xl my-10 '>
-          <div className='bg-white h-36 px-6 pt-8 text-sm'>
-            <p>{person.description}</p>
+          <div
+            key={index}
+            className='flex flex-col justify-center items-center mx-4 rounded border-2 shadow-2xl my-10 w-full h-64' data-aos="fade-up" delay="2000">
+            <div className=' h-36 pt-8 text-sm bg-blue-100'>
+              <p className='text-center px-4'>{person.description}</p>
+            </div>
+            <div className='absolute bottom-16'>
+              <Image className='w-20 h-20 rounded-full border-4 border-sky-100' src={person.image} width={200} height={200} alt='testimonials' />
+            </div>
+            <div className='text-center h-28 pt-16 text-sm' >
+              <p>{person.name}</p>
+              {/* <h2>{person.designation}</h2> */}
+            </div>
           </div>
-          <div className='absolute'>
-            <Image className='w-20 h-20 rounded-full border-4 border-sky-100' src={person.image} width={200} height={200} alt='testimonials' />
-          </div>
-          <div className='h-36 pt-14 px-6 text-sm font-bold '>
-            <h1>{person.name}</h1>
-            {/* <h2>{person.designation}</h2> */}
-          </div>
-        </div>
-      ))}
+        ))}
      </div>
     </div>
   );
