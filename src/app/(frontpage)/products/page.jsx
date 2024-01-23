aae081f00ca545f78072bd2e21978c35075d3725
adnanalemran <adnanalemranontor@gmail.com>
2024-01-23 16:34:44 +0600

    discover and add product form

--- src/app/(frontpage)/products/page.jsx
+++ src/app/(frontpage)/products/page.jsx
@@ -1,12 +1,28 @@
-import React from 'react';
+"use client";
+import AddProduct from "@/components/Product/AddProduct";
+import React from "react";
 
 const ProductPage = () => {
-    return (
-        <div>
-            {/* make a modal for add product  */}
-            <button>Add New Product</button>
+  return (
+    <div>
+      <button>Add New Product</button>
+
+      <button
+        className="btn"
+        onClick={() => document.getElementById("my_modal_1").showModal()}
+      >
+        open modal
+      </button>
+
+      <dialog id="my_modal_1" className="modal">
+        <div className="modal-box m-0 bg-base-300">
+          <div className="modal-action">
+            <AddProduct />
+          </div>
         </div>
-    );
+      </dialog>
+    </div>
+  );
 };
 
 export default ProductPage;
