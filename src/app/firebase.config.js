62da64f885849438442d57e12fc26ca3cbbb3da2
ashiq73 <ashiq.buet73@gmail.com>
2024-01-23 16:08:36 +0600

    firebase env data

--- src/app/firebase.config.js
+++ src/app/firebase.config.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyChe3VwslABEmZ9S3WhNYPSJznPO8o9xms",
-  authDomain: "expo-elite.firebaseapp.com",
-  projectId: "expo-elite",
-  storageBucket: "expo-elite.appspot.com",
-  messagingSenderId: "3637542514",
-  appId: "1:3637542514:web:ecf2ea198c842845950ceb",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
