876027951aefc4e8303b582eefc6a19af546850c
ashiqee <ashiq.buet73@gmail.com>
2024-01-26 12:15:17 +0600

    login 2

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -2,14 +2,14 @@
 import React from "react";
 import { UserAuth } from "../context/AuthContext";
 import Login from "@/components/Login/Login";
-// import Login2 from "@/components/Login/Login2";
+import Login2 from "@/components/Login/Login2";
 
 const LoginPage = () => {
   const { googleSignIn } = UserAuth();
   return (
     <div className="text-black">
-      <Login />
-      {/* <Login2 /> */}
+      {/* <Login /> */}
+      <Login2 />
       {/* <button onClick={googleSignIn} className="btn">
         Google login
       </button> */}
--- src/components/Login/Login2.jsx
+++ src/components/Login/Login2.jsx
@@ -1,7 +1,16 @@
 'use client'
 import Lottie from "lottie-react";
 import LoginAni from "./Login.json"
+import { UserAuth } from "@/app/(auth)/context/AuthContext";
 const Login2 = () => {
+
+    const {googleSignIn}=UserAuth()
+
+
+    const handleGoogleLogin =()=>{
+        googleSignIn()
+        
+    }
     return (
         <div className="min-h-screen ">
              <h1 className="text-3xl md:text-5xl font-bold text-center text-rose-600 my-10 ">Login</h1>
@@ -30,7 +39,7 @@ const Login2 = () => {
                     <div className="flex-1 h-px sm:w-16"></div>
                 </div>
                 <div className="flex justify-center space-x-4">
-                    <button aria-label="Log in with Google" className="p-3 rounded-sm text-rose-500">
+                    <button onClick={handleGoogleLogin} aria-label="Log in with Google" className="p-3 rounded-sm text-rose-500">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" className="w-5 h-5 fill-current">
                             <path d="M16.318 13.714v5.484h9.078c-0.37 2.354-2.745 6.901-9.078 6.901-5.458 0-9.917-4.521-9.917-10.099s4.458-10.099 9.917-10.099c3.109 0 5.193 1.318 6.38 2.464l4.339-4.182c-2.786-2.599-6.396-4.182-10.719-4.182-8.844 0-16 7.151-16 16s7.156 16 16 16c9.234 0 15.365-6.49 15.365-15.635 0-1.052-0.115-1.854-0.255-2.651z"></path>
                         </svg>
