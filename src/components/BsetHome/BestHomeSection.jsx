ae4fb4c9f9b68f0c3d919775f386e954148fafa1
ashiq73 <ashiq.buet73@gmail.com>
2024-01-31 13:12:53 +0600

    product details and navbar issue solved

--- src/components/BsetHome/BestHomeSection.jsx
+++ src/components/BsetHome/BestHomeSection.jsx
@@ -192,8 +192,10 @@ const BestHomeSection = () => {
                     </div>
                   </div>
                   <div className="w-1/4">
-                  <Link href={`/${property.id}`} > <button className="btn btn-1  btn-sm">view</button></Link>
-                   
+                    <Link href={`/products/${property.id}`}>
+                      {" "}
+                      <button className="btn btn-1  btn-sm">view</button>
+                    </Link>
                   </div>
                 </div>
               </div>
