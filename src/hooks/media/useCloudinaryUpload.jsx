0c96d49b025d4d42ae3490e66b7851b3b4551001
rayhanalmim <rayhanalmim1@gmail.com>
2024-02-01 19:31:08 +0600

    integrade cloudinary

--- src/hooks/media/useCloudinaryUpload.jsx
+++ src/hooks/media/useCloudinaryUpload.jsx
@@ -2,7 +2,7 @@ import { useState, useCallback } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import { Cloudinary } from 'cloudinary-core';
 
-const cloudinary = new Cloudinary({ cloud_name: 'deqkxg249' });
+const cloudinary = new Cloudinary({ cloud_name: 'dnjbmak75' });
 
 const useCloudinaryUpload = () => {
   const [image, setImage] = useState(null);
@@ -20,12 +20,13 @@ const useCloudinaryUpload = () => {
       // Upload image to Cloudinary
       const formData = new FormData();
       formData.append('file', file);
-      formData.append('upload_preset', 'your_upload_preset'); // Replace with your Cloudinary upload preset
+      formData.append('upload_preset', 'kjttiq0p'); // Replace with your Cloudinary upload preset
 
-      const response = await fetch(`https://api.cloudinary.com/v1_1/${cloudinary.config().deqkxg249}/image/upload`, {
+      const response = await fetch(`https://api.cloudinary.com/v1_1/dnjbmak75/image/upload`, {
         method: 'POST',
         body: formData,
       });
+      
 
       if (!response.ok) {
         throw new Error('Image upload failed');
@@ -35,8 +36,6 @@ const useCloudinaryUpload = () => {
 
       // Set the live URL
       const liveUrl = cloudinary.url(result.public_id, {
-        width: 150, // Set your preferred image width
-        height: 150, // Set your preferred image height
         crop: 'fill',
       });
 
