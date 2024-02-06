19825ae3211cff07a91a72db8fcb28201ffe3ab6
aubonee <chytahsin.2210@gmail.com>
2024-02-06 22:41:03 +0600

    Total property vs sold property chart added

--- src/app/(frontpage)/dashboard/page.jsx
+++ src/app/(frontpage)/dashboard/page.jsx
@@ -1,10 +1,16 @@
 
+
+import PropertyVsSold from '@/components/Dashboard/Seller/PropertyVsSold';
 import React from 'react';
 
 const DashBoard = () => {
     return (
         <div>
           dashboard
+          <div className='w-full'>
+          <PropertyVsSold />
+          </div>
+         
         </div>
     );
 };
