5da02c709943b744abd8e6627e49080da78c076a
aubonee <chytahsin.2210@gmail.com>
2024-02-07 11:10:57 +0600

    admin dashboard layout modified

--- src/components/Dashboard/Admin/AdminDetails.jsx
+++ src/components/Dashboard/Admin/AdminDetails.jsx
@@ -5,13 +5,16 @@ const AdminDetails = () => {
     return (
         <div>
             <div className="max-w-xs p-6 rounded-md shadow-md flex flex-col justify-center items-center bg-gradient-to-r from-[#F989B0] via-[#e49797] to-[#ffffff] dark:bg-gray-900 dark:text-gray-50 border-[#F43F5E] border-2">
-	<Image width={50} height={50} src="https://source.unsplash.com/random/300x300/?1" alt="admin" className="w-[70px] h-[70px] object-center  rounded-full  dark:bg-gray-500" />
+	<Image width={60} height={60} src="https://source.unsplash.com/random/300x300/?1" alt="admin" className="w-[70px] h-[70px] object-center  rounded-full  dark:bg-gray-500" />
 	<div className="mt-6 mb-2 text-center">
 		
 		<h2 className="text-xl font-semibold tracki">Tahsin Tarannum Chowdhury</h2>
-        <h2 className="text-xl font-semibold text-gray-500">tahsin@gmail.com</h2>
+        <h2 className="text-xl font-semibold text-gray-700">tahsin@gmail.com</h2>
+        <h2 className="text-xl font-semibold text-gray-600">Admin</h2>
 	</div>
-	<p className="dark:text-gray-100">Mauris et lorem at elit tristique dignissim et ullamcorper elit. In sed feugiat mi. Etiam ut lacinia dui.</p>
+	<div className="dark:text-gray-100">
+
+    </div>
 </div>
         </div>
     );
