d20156651497a0d99b9422c2ab79a5d491978386
ashiq73 <ashiq.buet73@gmail.com>
2024-02-07 21:33:16 +0600

    server cmnt

--- src/pages/api/products/post_products.js
+++ src/pages/api/products/post_products.js
@@ -1,25 +1,25 @@
-import { connectMongoDB } from "@/libs/MongoConnect";
-import productsCollection from "@/models/products";
+// import { connectMongoDB } from "@/libs/MongoConnect";
+// import productsCollection from "@/models/products";
 
-export default async function handler(req, res) {
+// export default async function handler(req, res) {
     
-  // res.status(201).send("Hi Server Running");
-  if (req.method !== 'POST') {
-      res.status(405).send({msg: "Only Post Reqeust Allow"});
-      return;
-    }
+//   // res.status(201).send("Hi Server Running");
+//   if (req.method !== 'POST') {
+//       res.status(405).send({msg: "Only Post Reqeust Allow"});
+//       return;
+//     }
     
-    const {product}=req.body;
+//     const {product}=req.body;
 
-    try{
-      await connectMongoDB();
-      productsCollection.create({product}).then((data)=>{
-        console.log(data);
-        res.status(201).send(data);
-      });
-    }catch(err){
-      console.log(err);
-      res.status(400).send({err,msg:"Something Worng"})
-    }
+//     try{
+//       await connectMongoDB();
+//       productsCollection.create({product}).then((data)=>{
+//         console.log(data);
+//         res.status(201).send(data);
+//       });
+//     }catch(err){
+//       console.log(err);
+//       res.status(400).send({err,msg:"Something Worng"})
+//     }
   
-  }
+//   }
