8ed2deef9c30925c8b98c3ab77b116bbd2d52fd0
husneara <husnearaa99@gmail.com>
2024-02-07 16:16:19 +0600

    has updateed seller and buyer style

--- src/components/Dashboard/Admin/PropertyVsSold.jsx
+++ src/components/Dashboard/Admin/PropertyVsSold.jsx
@@ -1,6 +1,6 @@
 "use client"
 import React from 'react';
-import  { PureComponent } from 'react';
+import { PureComponent } from 'react';
 import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 
 const data = [
@@ -8,78 +8,78 @@ const data = [
     name: 'Page A',
     totalSoldProperty: 2400,
     totalProperty: 4200,
-    
+
   },
   {
     name: 'Page B',
     totalSoldProperty: 1300,
     totalProperty: 3098,
-   
+
   },
   {
     name: 'Page C',
     totalSoldProperty: 2000,
     totalProperty: 9800,
-    
+
   },
   {
     name: 'Page D',
     totalSoldProperty: 2780,
     totalProperty: 3908,
-    
+
   },
   {
     name: 'Page E',
     totalSoldProperty: 1890,
     totalProperty: 4800,
-   
+
   },
   {
     name: 'Page F',
     totalSoldProperty: 2390,
     totalProperty: 3800,
-   
+
   },
   {
     name: 'Page G',
     totalSoldProperty: 3490,
     totalProperty: 4300,
-    
+
   },
 ];
 const PropertyVsSold = () => {
-    
-    // const dataForChart = payments.map(payment => ({
-    //     ...payment,
-    //     monthYear: `${payment.month}-${payment.year}`, // Combine month and year
-    //   }));
-    return (
-       
 
-<div className='w-full'>
-{/* <ResponsiveContainer width="100%" height="100%"> */}
-        <AreaChart
-         width={700} 
-         height={500}
-          data={data}
-          margin={{
-            top: 10,
-            right: 30,
-            left: 0,
-            bottom: 0,
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <Area type="monotone" dataKey="totalProperty" stackId="1"  stroke="#F43F5E" fill="#FECDD3"/>
-          <Area type="monotone" dataKey="totalSoldProperty" stackId="1" stroke="#E11D48" fill="#F989B0"  />
-          
-        </AreaChart>
+  // const dataForChart = payments.map(payment => ({
+  //     ...payment,
+  //     monthYear: `${payment.month}-${payment.year}`, // Combine month and year
+  //   }));
+  return (
+
+
+    <div className='w-full'>
+      {/* <ResponsiveContainer width="100%" height="100%"> */}
+      <AreaChart
+        width={700}
+        height={500}
+        data={data}
+        margin={{
+          top: 10,
+          right: 30,
+          left: 0,
+          bottom: 0,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Area type="monotone" dataKey="totalProperty" stackId="1" stroke="#F43F5E" fill="#FECDD3" />
+        <Area type="monotone" dataKey="totalSoldProperty" stackId="1" stroke="#E11D48" fill="#F989B0" />
+
+      </AreaChart>
       {/* </ResponsiveContainer> */}
-        </div>
-    );
+    </div>
+  );
 };
 
 export default PropertyVsSold;
--- src/components/Dashboard/Admin/TotalSeller.jsx
+++ src/components/Dashboard/Admin/TotalSeller.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 import { PieChart, Pie, Cell, Label } from 'recharts';
 
 const data = [
-    { name: 'Group A', value: 600 },
-    { name: 'Group B', value: 300 },
+    { name: 'Group A', value: 250 },
+    { name: 'Group B', value: 200 },
 
 ];
 const COLORS = ['#FF385D', '#FBD5EB'];
 
-const TotalProperty2 = () => {
+const TotalSeller = () => {
     return (
         <div className="p-6 rounded-md shadow-md flex flex-col justify-center items-center bg-gradient-to-r from-[#F989B0] via-[#e49797] to-[#ffffff] dark:bg-gray-900 dark:text-gray-50 border-[#F43F5E] border-2">
             <PieChart width={200} height={160} >
@@ -26,11 +26,11 @@ const TotalProperty2 = () => {
                     {data.map((entry, index) => (
                         <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
                     ))}
-                    <Label value="Total Property" position="center" className='text-black text-sm font-bold' />
+                    <Label value="Total Seller" position="center" className='text-black text-sm font-bold' />
                 </Pie>
             </PieChart>
         </div>
     );
 };
 
-export default TotalProperty2;
+export default TotalSeller;
