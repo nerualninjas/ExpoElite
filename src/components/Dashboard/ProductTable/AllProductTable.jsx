12988fa1739c845e215b8c7b75bff970bc9791f3
husneara <husnearaa99@gmail.com>
2024-02-09 18:36:00 +0600

    has updated some style and code

--- src/components/Dashboard/ProductTable/AllProductTable.jsx
+++ src/components/Dashboard/ProductTable/AllProductTable.jsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React, { useEffect } from 'react';
 import Image from 'next/image';
 import usePropertyData from '@/hooks/Propertys/usePropertyData';
@@ -15,21 +15,20 @@ const AllProductTable = () => {
     }, [properties]); // Make sure to add properties to the dependency array
 
     return (
-        <div>
-            <div className="flex items-center justify-between w-full">
-                <h2 className="text-lg font-semibold">All Property</h2>
-                <button className="px-4 py-2 bg-rose-400 text-black rounded-lg hover:bg-rose-600">Refresh</button>
+        <div className='bg-rose-50 rounded-lg'>
+            <div className="flex items-center justify-between w-full p-8 rounded-lg">
+                <h2 className="text-xl font-semibold">All Property</h2>
+                <button className="px-4 py-2 font-medium bg-rose-600 text-white rounded-lg hover:bg-rose-200">Refresh</button>
             </div>
-
             <div className="overflow-x-auto">
                 <table className="table mx-5 w-full mt-5 mb-10 pb-6">
                     {/* head */}
                     <thead>
                         <tr>
-                            <th></th>
+                            <th>No</th>
                             <th>Image</th>
                             <th>Property Name</th>
-                            <th>Seller Name</th>
+                            {/* <th>Seller Name</th> */}
                             <th>Details</th>
                             <th>Action</th>
                             <th>Action</th>
@@ -44,12 +43,12 @@ const AllProductTable = () => {
                                     <Image
                                         width={100}
                                         height={100}
-                                        src={`/images/${property.productPhoto}`}
+                                        src={`/images/${property.image}`}
                                         alt="image"
                                     />
                                 </td>
                                 <td>{property.propertyName}</td>
-                                <td>{property.SellerName}</td>
+                                {/* <td>{property.SellerName}</td> */}
                                 <td>
                                     <Link href={`products/${property._id}`}>
                                         <button className="btn btn-1 text-white bg-rose-500 btn-sm">View</button>
