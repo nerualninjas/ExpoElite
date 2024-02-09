1b81b136b9f62bff8e8e466fcb1b4db717efe22f
husneara <husnearaa99@gmail.com>
2024-02-09 19:20:45 +0600

    has updated chat name and image and in allproducttable has updated some code

--- src/components/Dashboard/ProductTable/AllProductTable.jsx
+++ src/components/Dashboard/ProductTable/AllProductTable.jsx
@@ -1,14 +1,14 @@
 "use client";
 import React, { useEffect } from 'react';
 import Image from 'next/image';
-import usePropertyData from '@/hooks/Propertys/usePropertyData';
 import Link from 'next/link';
+import usePropertyAllData from '@/hooks/Propertys/usePropertyAllData';
 
 const AllProductTable = () => {
 
-    const { AllProductData } = usePropertyData();
-    console.log(AllProductData);
-    const properties = AllProductData;
+    const { propertyData, isPending, refetch } = usePropertyAllData();
+    console.log('hello', propertyData);
+    const properties = propertyData;
 
     useEffect(() => {
         console.log(properties); // Log properties to the console
@@ -18,7 +18,7 @@ const AllProductTable = () => {
         <div className='bg-rose-50 rounded-lg'>
             <div className="flex items-center justify-between w-full p-8 rounded-lg">
                 <h2 className="text-xl font-semibold">All Property</h2>
-                <button className="px-4 py-2 font-medium bg-rose-600 text-white rounded-lg hover:bg-rose-200">Refresh</button>
+                {/* <button className="px-4 py-2 font-medium bg-rose-600 text-white rounded-lg hover:bg-rose-200">Refresh</button> */}
             </div>
             <div className="overflow-x-auto">
                 <table className="table mx-5 w-full mt-5 mb-10 pb-6">
@@ -28,7 +28,7 @@ const AllProductTable = () => {
                             <th>No</th>
                             <th>Image</th>
                             <th>Property Name</th>
-                            {/* <th>Seller Name</th> */}
+                            <th>Seller Info</th>
                             <th>Details</th>
                             <th>Action</th>
                             <th>Action</th>
@@ -43,19 +43,20 @@ const AllProductTable = () => {
                                     <Image
                                         width={100}
                                         height={100}
-                                        src={`/images/${property.image}`}
+                                        src={property.image}
                                         alt="image"
                                     />
                                 </td>
                                 <td>{property.propertyName}</td>
                                 {/* <td>{property.SellerName}</td> */}
+                                <td>selena12@gmail.com</td>
                                 <td>
-                                    <Link href={`products/${property._id}`}>
-                                        <button className="btn btn-1 text-white bg-rose-500 btn-sm">View</button>
+                                    <Link href={`/products/${property._id}`}>
+                                        <button className="btn text-white bg-rose-500">View</button>
                                     </Link>
                                 </td>
                                 <td>
-                                    <button className="btn text-white bg-[#53e068]">Approve</button>
+                                    <button className="btn text-white bg-[#53e068]">Publish</button>
                                 </td>
                                 <td>
                                     <button className="btn text-white bg-[#eb4343]">Reject</button>
