455d27a7166ebeedf7fe14d709d700ffe685554f
sabrinara <saashid18@gmail.com>
2024-02-13 16:47:32 +0600

    dynamically called user + userdashboard is working

--- src/components/Dashboard/Users/UserGraph.jsx
+++ src/components/Dashboard/Users/UserGraph.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import TotalPropertyVsTotalSell from './TotalPropertyVsTotalSell';
+import UserProfile from './UserProfile';
 
 const UserGraph = () => {
     return (
         <div>
-            
+            <UserProfile />
+            <TotalPropertyVsTotalSell />
         </div>
     );
 };
