8036d51969c34404b85f561720fbbae118977591
sabrinara <saashid18@gmail.com>
2024-02-13 17:22:23 +0600

    like and comment design done

--- src/components/Dashboard/Users/UserGraph.jsx
+++ src/components/Dashboard/Users/UserGraph.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import TotalPropertyVsTotalSell from './TotalPropertyVsTotalSell';
 import UserProfile from './UserProfile';
+import LikeComment from './LikeComment';
 
 const UserGraph = () => {
     return (
         <div>
             <UserProfile />
+            <LikeComment />
             <TotalPropertyVsTotalSell />
         </div>
     );
