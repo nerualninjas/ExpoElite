b95e045d079e2d8e63d61d38ac9b352b10d25394
ashiq73 <ashiq.buet73@gmail.com>
2024-02-14 18:45:44 +0600

    private route in two page done

--- src/models/user.js
+++ src/models/user.js
@@ -1,25 +1,25 @@
-const { Schema, model } = require('mongoose');
+// const { Schema, model } = require('mongoose');
 
-// ------------------------------------UsingSchema----------------------------------------
-const UserSchema = new Schema({
-    userName: {
-      type: String,
-      required: true,
-    },
-    userEmail: {
-      type: String,
-      required: true,
-      unique: true,
-      lowercase: true,
-      trim: true,
-    },
-    userRole: {
-      type: String,
-      enum: ['Admin', 'User', 'Manager'], 
-      required: true,
-    }
-  });
+// // ------------------------------------UsingSchema----------------------------------------
+// const UserSchema = new Schema({
+//     userName: {
+//       type: String,
+//       required: true,
+//     },
+//     userEmail: {
+//       type: String,
+//       required: true,
+//       unique: true,
+//       lowercase: true,
+//       trim: true,
+//     },
+//     userRole: {
+//       type: String,
+//       enum: ['Admin', 'User', 'Manager'], 
+//       required: true,
+//     }
+//   });
 
-const userCollection = model('users', UserSchema);
+// const userCollection = model('users', UserSchema);
 
-module.exports=userCollection;
+// module.exports=userCollection;
