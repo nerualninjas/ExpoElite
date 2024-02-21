d07a0871f82291bc8808ae1d9dd8444a593d84e4
ashiq73 <ashiq.buet73@gmail.com>
2024-02-21 20:12:23 +0600

    modal seller details optional

--- src/components/Dashboard/Admin/sellerTable/SellerDetailsModal.jsx
+++ src/components/Dashboard/Admin/sellerTable/SellerDetailsModal.jsx
@@ -19,12 +19,12 @@ const SellerDetailsModal =({data})=>{
               <Card className="py-4">
       <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
        <div>
-        <div> <p className="text-tiny uppercase font-bold">{data.membership}</p>
+        <div> <p className="text-tiny uppercase font-bold">{data?.membership}</p>
         <h4 className="font-bold text-large">{data?.userName}</h4>
         <small className="text-default-500">{data?.userEmail}</small></div>
         <div className="py-2">
-        <p className="text-tiny uppercase font-bold">Registration Date: {data.sellerRegStartDate.slice(0,10)}</p>
-        <p className="text-tiny uppercase font-bold">Expire Date: {data.sellerExpireDate.slice(0,10)}</p>  
+        <p className="text-tiny uppercase font-bold">Registration Date: {data?.sellerRegStartDate?.slice(0,10)}</p>
+        <p className="text-tiny uppercase font-bold">Expire Date: {data?.sellerExpireDate?.slice(0,10)}</p>  
         </div>
 
        </div>
