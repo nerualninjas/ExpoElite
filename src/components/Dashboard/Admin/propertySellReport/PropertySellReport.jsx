d1ed72fd2b98347d576b1121f7297ddd1e902805
husneara <husnearaa99@gmail.com>
2024-02-23 09:19:32 +0600

    has added the pagination ui and table data convertion

--- src/components/Dashboard/Admin/propertySellReport/PropertySellReport.jsx
+++ src/components/Dashboard/Admin/propertySellReport/PropertySellReport.jsx
@@ -1,15 +1,21 @@
 "use client";
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import Link from 'next/link';
 import useGetSoldProperty from '@/hooks/soldProperty/useGetSoldProperty';
-
-
+import { useReactToPrint } from 'react-to-print';
+import Swal from 'sweetalert2';
 
 const PropertySellReport = () => {
 
     const { soldPropertyData, isPending, refetch } = useGetSoldProperty();
     console.log('hello', soldPropertyData);
     const soldProperties = soldPropertyData;
+
+    // react-to-print
+    const componentPDF = useRef();
+
+
+
 
     useEffect(() => {
         console.log(soldProperties); // Log properties to the console
@@ -19,13 +25,24 @@ const PropertySellReport = () => {
 
 
 
+    // generatePDF
+    const generatePDF = useReactToPrint({
+        content: () => componentPDF.current,
+        documentTitle: "Invoicedata",
+        onAfterPrint: () => Swal.fire({
+            title: "done!",
+            text: "Invoice has download successfully!",
+            icon: "success"
+        })
+    });
+
     return (
         <div className='bg-rose-50 rounded-xl'>
             <div className="flex items-center justify-between w-full p-8 rounded-lg">
                 <h2 className="text-xl font-semibold">All Property Sell Report</h2>
                 {/* <button className="px-4 py-2 font-medium bg-rose-600 text-white rounded-lg hover:bg-rose-200">Refresh</button> */}
             </div>
-            <div className="overflow-x-auto">
+            <div ref={componentPDF} style={{ width: '100%' }} className="overflow-x-auto">
                 <table className="table mx-5 w-full mt-5 mb-10 pb-6">
                     {/* head */}
                     <thead>
@@ -72,6 +89,7 @@ const PropertySellReport = () => {
                                 <td>
                                     <button
                                         className="btn text-white bg-[#6090e9] hover:bg-[#4053a8] transition duration-700 ease-in-out"
+                                        onClick={generatePDF}
                                     >Print Invoice</button>
                                 </td>
                             </tr>
