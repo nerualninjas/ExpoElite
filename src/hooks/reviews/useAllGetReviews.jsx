4b7981c7d419fc559caca0fd865a7884fa1bdc4e
sabrinara <saashid18@gmail.com>
2024-02-23 14:00:30 +0600

    all reviews shows in the homepage

---
+++ src/hooks/reviews/useAllGetReviews.jsx
@@ -0,0 +1,28 @@
+"use client";
+import { UserAuth } from '@/app/(auth)/context/AuthContext';
+
+import { useQuery } from '@tanstack/react-query';
+import useAxiosPublic from '../useAxiosPublic';
+
+const useAllGetReviews = () => {
+    const { loading } = UserAuth();
+    const axiosPublic = useAxiosPublic();
+
+    const {
+        data: AllReviews,
+        isPending,
+        refetch,
+    } = useQuery({
+        queryKey: ["AllReviews"],
+        enabled: !loading,
+        queryFn: async () => {
+            const res = await axiosPublic.get('/getAllReviews');
+            console.log(res?.data);
+            return res?.data;
+        }
+    });
+
+    return { AllReviews, isPending, refetch };
+};
+
+export default useAllGetReviews;
