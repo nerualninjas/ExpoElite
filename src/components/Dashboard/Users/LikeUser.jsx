5db32c746f747b9306b5a985fdfe96b9a9ebb2f9
sabrinara <saashid18@gmail.com>
2024-02-24 22:07:56 +0600

    A user total comments in the userdashboard

--- src/components/Dashboard/Users/LikeUser.jsx
+++ src/components/Dashboard/Users/LikeUser.jsx
@@ -5,14 +5,12 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import useAUserLike from '@/hooks/users/useAUserLike';
 const LikeUser = () => {
     const { MyLikes } = useAUserLike();
-    console.log(MyLikes);
     const count = MyLikes?.totalLikes;
-
     
     return (
         
                     <div className='flex justify-between items-center bg-gray-100 p-10 rounded-md gap-4  shadow-md' >
-                        <FontAwesomeIcon className='rounded-full text-rose-400 w-16 h-16' icon= {faHeart} />
+                        <FontAwesomeIcon className='rounded-full text-rose-400 w-14 h-14' icon= {faHeart} />
                         <div className='flex flex-col justify-between items-center text-center'>
                             <p className='text-rose-600 '>Likes</p>
                             <p className='text-rose-600 text-2xl'>{count}</p>
--- src/components/Dashboard/Users/UserGraph.jsx
+++ src/components/Dashboard/Users/UserGraph.jsx
@@ -4,14 +4,19 @@ import UserProfile from './UserProfile';
 import SelledPropertyLocation from './SelledPropertyLocation';
 import LikeUser from './LikeUser';
 import PurchaseUser from './PurchaseUser';
+import CommentUser from './CommentUser';
+import Title2 from '@/components/shared/Title/Title2';
 
 const UserGraph = () => {
     return (
-        <div>
-            <div className='flex flex-col md:flex-row justify-center items-center gap-5 mb-10'>
+        <div className='p-10'>
+            <Title2 title='User Dashboard' />
+            <div className='flex flex-col md:flex-row justify-center items-center gap-3 mb-10'>
                 <UserProfile className='w-full md:w-1/2 ' />
-                <LikeUser className='w-full md:w-1/2' />
+                <LikeUser className='w-full md:w-1/2' /> 
+                <CommentUser className='w-full md:w-1/2' />
                 <PurchaseUser className='w-full md:w-1/2' />
+               
             </div>
             <div className='flex flex-col md:flex-row justify-center items-center gap-5'>
                 <TotalPropertyVsTotalSell className='w-full md:w-1/2 ' />
