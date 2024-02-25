50e836d3c5ba894a9e7f7b09abc52a2d0ab4489c
aubonee <chytahsin.2210@gmail.com>
2024-02-25 11:51:50 +0600

    My appointment page added

---
+++ src/app/(frontpage)/myAppointments/page.jsx
@@ -0,0 +1,12 @@
+import UserAppointments from '@/components/Dashboard/Seller/appointments/UserAppointments';
+import React from 'react';
+
+const MyAppointments = () => {
+    return (
+        <div>
+            <UserAppointments />
+        </div>
+    );
+};
+
+export default MyAppointments;
