fc7303ca93713d029aa06062dc22e91666168043
aubonee <chytahsin.2210@gmail.com>
2024-02-25 15:22:38 +0600

    My appointment page modified

--- src/components/Dashboard/Seller/appointments/UserAppointments.jsx
+++ src/components/Dashboard/Seller/appointments/UserAppointments.jsx
@@ -10,7 +10,7 @@ const getStatusColor = (status) => {
       case 'approved':
         return 'green';
       case 'pending':
-        return 'blue';
+        return '#30A9F2';
       case 'rejected':
         return 'red';
       default:
@@ -27,53 +27,59 @@ const UserAppointments = () => {
 
     return (
         <div>
-            <div className="overflow-x-auto bg-base-200">
-                <table className="table mx-5 w-full mt-5 mb-10 pb-6">
-                    {/* head */}
-                    <thead>
-                        <tr >
-                            <th>#</th>
-                           
-                            <th>Seller Email</th>
-                            <th> Seller Name</th>
-                            <th> User Image</th>
-                            <th>Appointment Date</th>
-                            <th>Appointment Time</th>
-                            <th>Appointment Status</th>
+              {userAppointmentData?.length > 0 ?(
 
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {userAppointmentData?.map((appointment, index) => (
-                            <tr key={appointment?._id}>
-                                <th>{index + 1}</th>
-                              
-                                <th>{appointment.sellerEmail}</th>
-                                <td>{appointment?.sellerName}</td>
-                                
-                                <td>
-                                    <Image
-                                        width={60}
-                                        height={60}
-                                        src={appointment?.userPhoto}
-                                        alt="image"
-                                        className="rounded-full w-[60px] h-[60px]"
-                                    />
-                                </td>
-                                <td>{appointment?.appointmentDate} </td>
-                                <td>{appointment?.appointmentStartTime}  - {appointment?.appointmentEndTime} </td>
-                                
-                                <td className='text-xl font-bold' style={{ color: getStatusColor(appointment?.appointmentStatus) }}>
-                  {appointment?.appointmentStatus}
+<div className="overflow-x-auto bg-base-200">
+<table className="table mx-5 w-full mt-5 mb-10 pb-6">
+    {/* head */}
+    <thead>
+        <tr className='text-lg text-rose-500'>
+            <th> </th>
+           
+            {/* <th>Seller Email</th> */}
+            <th> Seller Name</th>
+            <th>Property Location</th>
+            <th>Appointment Date</th>
+            <th>Appointment Time</th>
+            <th>Appointment Status</th>
+
+        </tr>
+    </thead>
+    <tbody>
+        {userAppointmentData?.map((appointment, index) => (
+            <tr key={appointment?._id}>
+                <th>{index + 1}</th>
+              
+                {/* <th>{appointment.sellerEmail}</th> */}
+                <td>{appointment?.sellerName}</td>
+                
+                <td>
+                    {/* <Image
+                        width={60}
+                        height={60}
+                        src={appointment?.userPhoto}
+                        alt="image"
+                        className="rounded-full w-[60px] h-[60px]"
+                    /> */}
+                    {appointment?.propertyName}
                 </td>
-                   
-                        
+                <td>{appointment?.appointmentDate} </td>
+                <td>{appointment?.appointmentStartTime}  - {appointment?.appointmentEndTime} </td>
+                
+                <td className='text-xl font-semibold' style={{ color: getStatusColor(appointment?.appointmentStatus) }}>
+  {appointment?.appointmentStatus}
+</td>
+   
+        
 
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </div>
+            </tr>
+        ))}
+    </tbody>
+</table>
+</div>
+              ):( <p className='text-center my-10 text-yellow-500 font-semibold text-xl'>You didn't book any appointment yet.</p>)
+              }
+          
         </div>
     );
 };
