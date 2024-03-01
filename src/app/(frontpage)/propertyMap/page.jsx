426ebb11965bcdb2b92889c90af1cdec74163d49
ashiq73 <ashiq.buet73@gmail.com>
2024-03-01 19:15:40 +0600

    property rename

---
+++ src/app/(frontpage)/propertyMap/page.jsx
@@ -0,0 +1,25 @@
+"use client"
+
+
+// import Map from "@/components/PropertyMapComp/Map/Map";
+
+const PropertyMapPage = () => {
+    
+
+    
+    return (
+        <div>
+            <h2 className="text-2xl font-bold"><span className="text-rose-500">Property</span> Map View </h2>
+            {/* list property  */}
+   
+        {/* Map view  */}
+       
+        { window !== 'undefined' && (
+{/* <Map  /> */}
+        )}
+        
+        </div>
+    );
+};
+
+export default PropertyMapPage;
