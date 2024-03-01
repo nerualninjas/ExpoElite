73e3d755f1d88db73a2e324950ffdbaf37ff8c78
ashiq73 <ashiq.buet73@gmail.com>
2024-03-01 17:26:20 +0600

    window not condition

--- src/components/PropertyMapComp/Map/Map.js
+++ src/components/PropertyMapComp/Map/Map.js
@@ -35,10 +35,12 @@ const Map = () => {
 
        {/* //scrollWheelZoom={false} */}
 <MapContainer center={position} zoom={12} > 
-    <TileLayer
-      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    />
+{typeof window !== 'undefined' && (
+  <TileLayer
+    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+  />
+)}
 <MarkerClusterGroup
 chunkedLoading
 >
