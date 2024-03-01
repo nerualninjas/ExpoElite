244ef2cc4ed3b7e224e46ce74063e91916a8c7be
ashiq73 <ashiq.buet73@gmail.com>
2024-03-01 18:47:24 +0600

    window undefine map page

--- src/app/(frontpage)/propertyInMap/page.jsx
+++ src/app/(frontpage)/propertyInMap/page.jsx
@@ -1,25 +1,25 @@
-"use client"
+// "use client"
 
 
-import Map from "@/components/PropertyMapComp/Map/Map";
+// import Map from "@/components/PropertyMapComp/Map/Map";
 
-const PropertyViewInMap = () => {
+// const PropertyViewInMap = () => {
     
 
     
-    return (
-        <div>
-            <h2 className="text-2xl font-bold"><span className="text-rose-500">Property</span> Map View </h2>
-            {/* list property  */}
+//     return (
+//         <div>
+//             <h2 className="text-2xl font-bold"><span className="text-rose-500">Property</span> Map View </h2>
+//             {/* list property  */}
    
-        {/* Map view  */}
+//         {/* Map view  */}
        
-        {typeof window !== 'undefined' && (
-<Map  />
-        )}
+//         { window !== 'undefined' && (
+// <Map  />
+//         )}
         
-        </div>
-    );
-};
+//         </div>
+//     );
+// };
 
-export default PropertyViewInMap;
+// export default PropertyViewInMap;
