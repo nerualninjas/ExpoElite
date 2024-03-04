1262c4fee80ea2371676b6487f6c9357b7b8b7bd
aubonee <chytahsin.2210@gmail.com>
2024-03-04 15:18:41 +0600

    modified banner

--- src/components/BsetHome/BestHomeSection.jsx
+++ src/components/BsetHome/BestHomeSection.jsx
@@ -59,7 +59,7 @@ const BestHomeSection = () => {
   };
   return (
     <>
-      <div className="w-full py-12">
+      <div className="w-full py-6">
         <div className="container mx-auto">
 
           {/* <h3 className="text-center w-100 text-xl font-bold text-gray-900 p-4 ">
