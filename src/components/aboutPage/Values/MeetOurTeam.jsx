8d2d9098e7459af029999e539169cee4b1c9463a
aubonee <chytahsin.2210@gmail.com>
2024-03-06 23:14:24 +0600

    user getting notification for seller Appointment approval or reject  is added

--- src/components/aboutPage/Values/MeetOurTeam.jsx
+++ src/components/aboutPage/Values/MeetOurTeam.jsx
@@ -1,6 +1,39 @@
 import Image from "next/image";
 
 const MeetOurTeam = () => {
+    const teamMembers = [
+        {
+            name: "Hosneara Popy",
+            email: "husnearaa99@gmail.com",
+            imageSrc: "https://i.ibb.co/37T8nVg/popyApu.jpg",
+        },
+        {
+            name: "Tahsin Tarannum Chy",
+            email: "chytahsin.2210@gmail.com",
+            imageSrc: "https://i.ibb.co/QFcpQ5Y/Tashin-Tarannum-Chy-Dpt-CSE.jpg",
+        },
+        {
+            name: "Adnan al-emran ontor",
+            email: "adnanalemran@hotmail.com",
+            imageSrc: "https://i.ibb.co/mq937CG/Adnan.png",
+        },
+        {
+            name: "Sabrina Rashid",
+            email: "sabrina.rashid.sara@gmail.com",
+            imageSrc: "https://i.ibb.co/RvJtjZD/429823336-448305350855105-1214185624739971414-n.png",
+        },
+        {
+            name: "Ashek Mahmud Ashik",
+            email: "aashekmahmud@gmail.com",
+            imageSrc: "https://i.ibb.co/wwHtB0v/a-SHIQ-IMAGE.png",
+        },
+        {
+            name: "Rayhan Al Mim",
+            email: "rayhanalmim1@gmail.com",
+            imageSrc: "https://i.ibb.co/D82rzvJ/rayhanalmim.webp",
+        },
+
+    ];
     return (
         <div>
             <div className=" px-4 py-8 mb-20">
@@ -9,8 +42,8 @@ const MeetOurTeam = () => {
       Meet Our Team
       </h2>
               
-                <div className="grid grid-cols-2 md:grid-cols-4  p-5 gap-3 justify-center mx-auto rounded-md">
-                    <div className="text-center space-y-1">
+                <div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-6  p-5 gap-3 justify-center mx-auto rounded-md">
+                    {/* <div className="text-center space-y-1">
                         <Image
                             className="mx-auto rounded-full object-cover h-32 w-32"
                             src="https://i.ibb.co/WpqdFH7/istockphoto-1419898772-170667a.webp"
@@ -20,67 +53,27 @@ const MeetOurTeam = () => {
                         />
                         <h2 className="text-xl font-semibold">John Smith</h2>
                         <p className="font-medium">Founder & CEO</p>
-                    </div>
-                    <div className="text-center space-y-1">
-                        <Image
-                            className="mx-auto rounded-full object-cover h-32 w-32"
-                            src="https://i.ibb.co/WpqdFH7/istockphoto-1419898772-170667a.webp"
-                            alt="My Image"
-                            width={300}
-                            height={300}
-                        />
-                        <h2 className="text-xl font-semibold">John Smith</h2>
-                        <p className="font-medium">Founder & CEO</p>
-                    </div>
-                    <div className="text-center space-y-1">
-                        <Image
-                            className="mx-auto rounded-full object-cover h-32 w-32"
-                            src="https://i.ibb.co/WpqdFH7/istockphoto-1419898772-170667a.webp"
-                            alt="My Image"
-                            width={300}
-                            height={300}
-                        />
-                        <h2 className="text-xl font-semibold">John Smith</h2>
-                        <p className="font-medium">Founder & CEO</p>
-                    </div>
-                    <div className="text-center space-y-1">
-                        <Image
-                            className="mx-auto rounded-full object-cover h-32 w-32"
-                            src="https://i.ibb.co/0Gzpbr1/istockphoto-1342247162-170667a.jpg"
-                            alt="My Image"
-                            width={300}
-                            height={300}
-                        />
-                        <h2 className="text-xl font-semibold">Emily Davis</h2>
-                        <p className="font-medium">Real Estate Agent</p>
-                    </div>
-                    <div className="text-center space-y-1">
-                        <Image
-                            className="mx-auto rounded-full object-cover h-32 w-32"
-                            src="https://i.ibb.co/41YGGp1/motivated-young-manager-suit-looking-camera-office-headshot-portrait-smiling-businessman-teacher-for.webp"
-                            alt="My Image"
-                            width={300}
-                            height={300}
-                        />
-                        <h2 className="text-xl font-semibold">Michael Rodriguez</h2>
-                        <p className="font-medium">Property Consultant</p>
-                    </div>
-                    <div className="text-center space-y-1">
-                        <Image
-                            className="mx-auto rounded-full object-cover h-32 w-32"
-                            src="https://i.ibb.co/tXX8xqz/istockphoto-1148808852-612x612.jpg"
-                            alt="My Image"
-                            width={300}
-                            height={300}
-                        />
-                        <h2 className="text-xl font-semibold">James Mitchell</h2>
-                        <p className="font-medium">Legal Advisor</p>
-                    </div>
+                    </div> */}
+                    {teamMembers.map((member, index) => (
+                        <div key={index} className="text-center space-y-1">
+                            <Image
+                                className="mx-auto rounded-full object-cover h-32 w-32"
+                                src={member.imageSrc}
+                                alt={`${member.name} Image`}
+                                width={300}
+                                height={300}
+                            />
+                            <h2 className="text-lg font-semibold">{member.name}</h2>
+                            <p className="text-sm">{member.email}</p>
+                        </div>
+                    ))}
+                   
 
+                   <div>
+           
+        </div>
                 </div>
-                {/* <div data-aos="flip-up" className="bg-slate-50 p-2">
-                    <p className="text-center">Meet the dedicated professionals behind <span className="tracking-widest font-semibold text-green-500">Expo Elite</span>, committed to guiding you through every step of your real estate journey. Whether you are buying, selling, or investing, our experienced team is here to turn your property dreams into reality. Your success is our priority.</p>
-                </div> */}
+                
             </div>
         </div>
     );
