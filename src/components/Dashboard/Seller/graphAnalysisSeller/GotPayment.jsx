167e193299d3ee2cd8d0b1a2b75809b2eaebc7ae
aubonee <chytahsin.2210@gmail.com>
2024-03-07 13:20:23 +0600

    added chart in userdashboard and modify homepage, seller Dashboard

--- src/components/Dashboard/Seller/graphAnalysisSeller/GotPayment.jsx
+++ src/components/Dashboard/Seller/graphAnalysisSeller/GotPayment.jsx
@@ -10,13 +10,13 @@ const GotPayment = () => {
     // console.log('totalAmount',sellerPayment?.totalAmount);
     return (
         
-        <div className="card shadow-md bg-base-100 px-4 py-10 justify-around items-center flex flex-col lg:flex-row">
+        <div className="card shadow-md bg-base-100 px-4 py-10 justify-around items-center flex flex-col xl:flex-row">
                 
           <div className="radial-progress text-rose-500" style={{"--value":"100", "--size": "4rem", "--thickness": "4px"}} role="progressbar"><FontAwesomeIcon className='text-xl bg-red-200 px-4 py-3 rounded-full' icon={faDollarSign} /></div>
 
           <div className='ml-2'>
-              <h2 className='text-xl font-semibold'> My Total Earning</h2>
-              <p className='text-lg font-semibold text-red-400 '> $ {sellerPayment?.totalAmount}</p>
+              <h2 className='text-lg font-semibold'> My Total Earning</h2>
+              <p className='text-xl font-semibold text-red-400 '> $ {sellerPayment?.totalAmount}</p>
           </div>
       </div>
 
