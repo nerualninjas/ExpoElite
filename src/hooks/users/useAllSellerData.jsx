97ab946f00ae8430d10154357fc4a0ea99f58ec8
husneara <husnearaa99@gmail.com>
2024-03-07 15:33:33 +0600

    has updated the seller user data pagination ui and has updated the for chatbot message feature

--- src/hooks/users/useAllSellerData.jsx
+++ src/hooks/users/useAllSellerData.jsx
@@ -3,24 +3,24 @@ import { UserAuth } from "@/app/(auth)/context/AuthContext";
 import { useQuery } from "@tanstack/react-query";
 import useAxiosPublic from "./../useAxiosPublic";
 const useAllSellerData = () => {
-    const { loading } = UserAuth();
-    const axiosPublic = useAxiosPublic();
-  
-    const {
-      data: AllSellerData,
-      isPending,
-      refetch,
-    } = useQuery({
-      queryKey: ["AllSellerData"],
-      enabled: !loading, // Don't fetch data during SSR
-      queryFn: async () => {
-        const res = await axiosPublic.get("/getSellers");
-        console.log(res?.data);
-        return res?.data;
-      },
-    });
-  
-    return { AllSellerData, isPending, refetch };
-  };
-  
-  export default useAllSellerData;
+  const { loading } = UserAuth();
+  const axiosPublic = useAxiosPublic();
+
+  const {
+    data: AllSellerData,
+    isPending,
+    refetch,
+  } = useQuery({
+    queryKey: ["AllSellerData"],
+    enabled: !loading, // Don't fetch data during SSR
+    queryFn: async () => {
+      const res = await axiosPublic.get("/getSellers");
+      console.log(res?.data);
+      return res?.data;
+    },
+  });
+  const AllSellerDataLength = AllSellerData?.length;
+  return { AllSellerData, AllSellerDataLength, isPending, refetch };
+};
+
+export default useAllSellerData;
