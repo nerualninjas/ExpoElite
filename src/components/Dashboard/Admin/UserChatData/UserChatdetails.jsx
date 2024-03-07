4a1b3edd0f89ae88d8a1eb88fb482bbe1e93e25c
rayhanalmim <rayhanalmim1@gmail.com>
2024-03-07 17:45:47 +0600

    comit

--- src/components/Dashboard/Admin/UserChatData/UserChatdetails.jsx
+++ src/components/Dashboard/Admin/UserChatData/UserChatdetails.jsx
@@ -1,25 +1,102 @@
+"use client"
 import Title2 from '@/components/shared/Title/Title2';
 import Image from 'next/image';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'next/navigation';
+import useAxiosPublic from '@/hooks/useAxiosPublic';
+import { useQuery } from '@tanstack/react-query';
+import { UserAuth } from '@/app/(auth)/context/AuthContext';
 
 const UserChatdetails = () => {
+    const { id } = useParams();
+    const axiosPublic = useAxiosPublic();
+    const { user } = UserAuth();
+
+    const { data: messageLogs, isLoading, isPending, refetch } = useQuery({
+        queryKey: ["singleChatData"],
+        queryFn: async () => {
+            const res = await axiosPublic.get(`/getMessage?id=${id}`);
+            console.log(res.data);
+            return res?.data;
+        },
+    });
+
+    const [message, setMessage] = useState('');
+    const [chat, setChat] = useState([]);
+
+    console.log(messageLogs);
+
+    useEffect(() => {
+        setChat(messageLogs?.chatLogs);
+        refetch();
+    }, [messageLogs, refetch]);
+
+
+    const handleSendMessage = async () => {
+        if (message.trim() !== '') {
+            console.log(message);
+            const res = await axiosPublic.post(`/sendMessage?senderEmail=${user.email}&user=${messageLogs.senderEmail}`, {
+                message: message
+            });
+            console.log(res.data);
+            refetch();
+            setMessage('');
+        }
+    };
+
+
+    if (isLoading) {
+        return 'loading';
+    }
+
     return (
-        <div>
-            <div className="bg-white text-black p-4 m-4 rounded-xl">
-                <Title2 title="Chat History Details " className=" mt-6" />
-                <div className='bg-rose-200 rounded py-4 px-4 md:px-5 my-10 md:mx-10'>
-                    <div className='flex flex-col md:flex-row justify-around items-center gap-4 my-6  p-4 bg-gray-100 rounded-lg'>
-                        <Image src={"https://loamic-media.s3.us-east-2.amazonaws.com/1709713184189-rayhanalmim.jpg"} alt="property image" className='rounded-xl' width={80} height={80} />
-                        <div >
-                            <p><span className="font-bold">Name</span></p>
-                        </div>
-                        <div className='flex items-center'>
-                            <p><span className="font-bold mr-2">Message</span></p>
-                        </div>
-                    </div>
+        <div className="w-full">
+            <div className=" bg-slate-100 border border-gray-300 rounded-lg overflow-hidden flex flex-col">
+                <div className="bg-rose-500 text-white p-2 flex items-center justify-start">
+                    <Image
+                        width={200}
+                        height={150}
+                        src={user?.photoURL}
+                        alt="Profile"
+                        className="w-8 h-8 rounded-full mr-2"
+                    />
+                    <span>{user?.displayName}</span>
+                </div>
+                {
+                    chat ? <div className="flex-1 p-4 overflow-y-auto bg-base-100">
+                        {chat.map((msg, index) => (
+                            <div key={index} className={`mb-2 ${msg.sendBy === user.email ? 'text-right' : 'text-left'}`}>
+                                <div
+                                    className={`inline-block p-2 rounded-lg ${msg.sendBy === user.email ? 'bg-rose-200 text-black' : 'bg-green-100 text-black'
+                                        }`}
+                                >
+                                    {msg.message}
+                                </div>
+                                <div className="text-xs text-gray-500 mt-1">
+                                    {msg.sendingTime}
+                                </div>
+                            </div>
+                        ))}
+                    </div> : "no chat"
+                }
+
+                <div className="p-2 flex items-center justify-between bg-gray-200">
+                    <input
+                        type="text"
+                        placeholder="Type a message..."
+                        className="flex-1 p-2 rounded"
+                        value={message}
+                        onChange={(e) => setMessage(e.target.value)}
+                    />
+                    <button
+                        onClick={handleSendMessage}
+                        className="bg-rose-500 text-white p-2 rounded ml-2"
+                    >
+                        Send
+                    </button>
                 </div>
             </div>
-        </div >
+        </div>
     );
 };
 
